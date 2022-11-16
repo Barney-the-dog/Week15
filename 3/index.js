e58d1ecbb902f86060f4990bba1f55801b032877
Ashurova Irina <IAshurova@SRF.local>
2022-11-16 22:19:48 +0700

    third

---
+++ 3/index.js
@@ -0,0 +1,14 @@
+function change() {
+    switch (document.getElementById("color").value) {
+        case "red": document.getElementById("doc").style.backgroundColor = "red";
+            break;
+    }
+    switch (document.getElementById("color").value) {
+        case "blue": document.getElementById("doc").style.backgroundColor = "blue";
+            break;
+    }
+    switch (document.getElementById("color").value) {
+        case "yellow": document.getElementById("doc").style.backgroundColor = "yellow";
+            break;
+    }
+}
